697e0fc36c0a424e96871439e8b3ff15ba7f4b38
priyavarshini-mediwave <priyavarshini@mindwaveventures.com>
2023-10-20 14:56:57 +0530

    put and delete

--- app/index.js
+++ app/index.js
@@ -5,10 +5,49 @@ const morgan = require("morgan");
 const app = express();
 app.use(morgan("combined"));
 app.use(express.json());
+const favs = [];
 app.get("/:str", (req, res) => {
-  const len = stringLength(req.params.str);
+  const len = stringLength("priya");
   console.log(req.params.str);
   return res.json({ message: "Helloworld", length: len });
+});
+app.get("/", (req, res) => {
+  //   res.json("Hello world");
+  console.log(favs);
+  res.json({
+    data: favs,
+  });
+});
+// });
+
+app.post("/", (req, res) => {
+  favs.push({
+    id: new Date().getTime(),
+    text: req.body,
+  });
+  //   let body = req.body;
+  res.json({ favs, message: "pushed" });
+  // message: body,
+});
+// });
+
+app.put("/", (req, res) => {
+  return res.json({
+    message: "PUT route",
+  });
+});
+
+app.put("/", (req, res) => {
+  return res.json({
+    message: "PUT route",
+  });
+});
+
+// DELETE
+app.delete("/", (req, res) => {
+  return res.json({
+    message: "DELETE route",
+  });
 });
 
 app.listen(port, () => {
